6c00dea02b1c2636218d5003454f554244347fb2
usacys <usacys@local>
2020-10-14 13:21:02 -0400

    Updated pe1 part5

--- pe1/part5_deliverable.py
+++ pe1/part5_deliverable.py
@@ -21,7 +21,7 @@ def read_pgm(filename):
     #Splits file into header and body elements
     for i in range(len(content)):
         if content[i] == '255':
-            temp = (content[:i + 1], content[i+ 1:])
+            temp = (content[:i + 1], content[i + 1:])
             return temp
 
 
@@ -38,19 +38,16 @@ def write_pgm(filename, content):
     """
     fout = open(filename, 'w')
 
-    #Writes the header
+    # Header
     for i in range(len(content[0])):
-        if (i == 1 and len(content[0]) == 4) or (i == 2 and len(content[0]) > 4):
-            fout.write(content[0][i])
-            fout.write(" ")
+        if i == 1:
+            fout.write(content[0][i] + " ")
         else:
-            fout.write(content[0][i])
-            fout.write("\n")
+            fout.write(content[0][i] + "\n")
 
-    #Writes the body
+    # Tail
     for i in content[1]:
-        fout.write(i)
-        fout.write("\n")
+        fout.write(i + "\n")
 
     fout.close()
 
