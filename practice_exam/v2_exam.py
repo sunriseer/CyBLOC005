fc291111161e47e46e8b985c73a453a9d8e2fd3a
usacys <usacys@local>
2020-10-21 13:09:13 -0400

    Added practice exam 2

--- practice_exam/v2_exam.py
+++ practice_exam/v2_exam.py
@@ -8,7 +8,12 @@ def q1(sentence):
         'it is accepted as a masterpiece on strategy', the returned
         string should be 'strategy on masterpiece a as accepted is it'.
     """
-    pass
+
+    temp = sentence.split(" ")
+    temp.reverse()
+
+    return " ".join(temp)
+
 
 def q2(n):
     """
@@ -16,7 +21,9 @@ def q2(n):
         commas seperating groups of 3 digits. For example, given 65535
         the returned string should be '65,535'.
     """
-    pass
+
+    return "{:,}".format(n)
+
 
 def q3(lst0, lst1):
     """
@@ -25,14 +32,24 @@ def q3(lst0, lst1):
         given [3,4,9] and [8,1,5] the returned list should be [9,8,5,4,3,1].
         The returned list may contain duplicates.
     """
-    pass
 
-def q4(s1,s2,s3):
+    temp = lst0+lst1
+    temp.sort(reverse=True)
+
+    return temp
+
+
+def q4(s1, s2, s3):
     """
         Given 3 scores in the range [0-100] inclusive, return 'GO' if
         the average score is greater than 50. Otherwise return 'NOGO'.
     """
-    pass
+
+    if (s1 + s2 + s3) / 3 > 50:
+        return "GO"
+    else:
+        return "NOGO"
+
 
 def q5(integer, limit):
     """
@@ -41,14 +58,35 @@ def q5(integer, limit):
         limit==30, the returned list should be [0,6,12,18,24,30]. Note, 0 is
         a multiple of any integer except 0 itself.
     """
-    pass
+
+    temp = []
+    i = 0
+
+    while i * integer <= limit:
+        if i * integer != 0 or integer != 0:
+            if i * integer % 2 == 0:
+                temp.append(i * integer)
+        i += 1
+
+    return temp
+
 
 def q6(f0, f1):
     """
         Given two filenames, return a list whose elements consist of line numbers
         for which the two files differ. The first line is considered line 0.
     """
-    pass
+
+    with open(f0, 'r') as fin0, open(f1, 'r') as fin1:
+        index = 0
+        temp = []
+
+        for line in fin0:
+            if line != fin1.readline():
+                temp.append(index)
+            index += 1
+
+    return temp
 
 def q7(lst):
     """
@@ -56,14 +94,29 @@ def q7(lst):
         For example, if given [5,7,9,1,3,7,9,5], the returned value should
         be 7.
     """
-    pass
+
+    seen = []
+
+    for i in lst:
+        if i in seen:
+            return i
+        seen.append(i)
+
 
 def q8(strng):
     """
         Given a sentence as a string with words being separated by a single space,
         return the length of the shortest word.
     """
-    pass
+
+    temp = len(strng.split(" ")[0])
+
+    for i in strng.split(" "):
+        if len(i) < temp:
+            temp = len(i)
+
+    return temp
+
 
 def q9(strng):
     """
@@ -73,7 +126,15 @@ def q9(strng):
         'hell9oworld7', the returned character should be 'a' which has
         the ascii value of 97.
     """
-    pass
+
+    temp = []
+
+    for i in strng:
+        if i.isdigit():
+            temp.append(i)
+
+    return chr(int("".join(temp)))
+
 
 def q10(arr):
     """
@@ -81,4 +142,8 @@ def q10(arr):
         the first non-consecutive value. If all values are consecutive, return
         None. For example, given [1,2,3,4,6,7], the returned value should be 6.
     """
-    pass
+
+    for i in range(len(arr) - 1):
+        if arr[i + 1] != arr[i] + 1:
+            return arr[i + 1]
+
