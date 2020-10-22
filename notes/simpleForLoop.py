73f0c6428a3af97cf8db768736b6a283a755d7f9
Kaleb McMillen <kaleb@my-macfam.com>
2020-10-22 09:58:52 -0400

    Just notes and things

---
+++ notes/simpleForLoop.py
@@ -0,0 +1,69 @@
+"""
+PI = 3.14159
+return 4 * PI * (radius ** 2)
+
+#######################################
+
+return False if 223 >= int(addr.split('.')[0]) >= 240 else True
+
+#######################################
+
+return [i for i in range(0, 1024)]
+
+#######################################
+
+if number[0] == 'z':
+    return 0
+elif number[0] == 'o':
+    return 1
+elif number[0] == 't':
+    if number[1] == 'w':
+        return 2
+    else:
+        return 3
+else:
+    return 4
+
+#######################################
+
+data = input("Enter an number: ")
+temp = []
+
+for i in range(len(data)):
+    if data[i].isdigit():
+        temp.append(data[i])
+
+return int(''.join(temp))
+
+#######################################
+
+print("{}.{}.{}@{}.com".format(first, middle[0], last, domain))
+
+#######################################
+
+with open(infile, "r") as fin, open(outfile, "w") as fout:
+    fout.writelines(fin.readlines())
+
+#######################################
+
+return tuple('.'.join(address.split('@')).split('.')[:4])
+
+#######################################
+
+
+
+#######################################
+
+
+
+#######################################
+
+
+
+#######################################
+
+
+
+#######################################
+
+"""
